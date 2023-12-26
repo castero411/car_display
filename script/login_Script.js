0cb0e49cfc88f40b5f993bc66d9e748b38cdc7ef
castero411 <ahking491@gmail.com>
2023-12-26 20:09:31 +0200

    edit 3

--- script/login_Script.js
+++ script/login_Script.js
@@ -1,4 +1,40 @@
-function submit(){
-    var text = document.getElementsByTagName("h2");
-    text.innetHTML = "welcome";
+
+function val() {
+    var user = document.getElementById("username");
+    var flag = 0;
+    var valid = false;
+
+
+    if (user.value === ""){
+        flag = 2;
+    }else{
+        flag = 0;
+    }
+
+    var pass = document.getElementById("password");
+
+    if(pass.value === ""){
+        flag+=1;
+    }
+    switch(flag){
+        case 0:
+            valid = true;
+            break;
+        
+        case 1 :
+            pass.style = "border-color:var(--red-pantone);";
+            pass.placeholder = "insert password";
+            break;
+        case 2:
+            user.style = "border-color:var(--red-pantone);";
+            user.placeholder = "insert a username";
+            break;
+        case 3:
+            user.style = "border-color:var(--red-pantone);";
+            user.placeholder = "insert a username";
+            pass.style = "border-color:var(--red-pantone);";
+            pass.placeholder = "insert password";
+            break;
+    }
+    return valid;
 }
