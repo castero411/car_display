410029477e1605920793bd58449e85a9354f874b
castero411 <ahking491@gmail.com>
2023-12-30 14:23:34 +0200

    edit 7

--- script/login_Script.js
+++ script/login_Script.js
@@ -48,3 +48,59 @@ function val() {
     
 }
 
+
+
+
+function val2() {
+
+    var mail = document.getElementById("email");
+    var flag = 0;
+    
+    if (mail.value === ""){
+        mail.style = "border-color:var(--red-pantone);";
+    }
+    else{}
+
+    var user = document.getElementById("username");
+    var flag = 0;
+    
+    if (user.value === ""){
+        flag = 2;
+    }else{
+        flag = 0;
+    }
+
+    var pass = document.getElementById("password");
+
+    if(pass.value === ""){
+        flag+=1;
+    }
+    switch(flag){
+        case 0:
+            valid = true;
+            window.location.href = "index.html";
+            break;
+        
+        case 1 :
+            pass.style = "border-color:var(--red-pantone);";
+            pass.placeholder = "insert password";
+            break;
+        case 2:
+            user.style = "border-color:var(--red-pantone);";
+            user.placeholder = "insert a username";
+            break;
+        case 3:
+            user.style = "border-color:var(--red-pantone);";
+            user.placeholder = "insert a username";
+            pass.style = "border-color:var(--red-pantone);";
+            pass.placeholder = "insert password";
+            break;
+    }
+    if (valid === true){
+        window.alert("password is valid");
+        window.location.href = "index.html";
+    }
+    
+    return valid;
+    
+}
